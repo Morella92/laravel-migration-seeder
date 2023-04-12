9adcc61279cad9e2801d02795286f017264e1500
Morella92 <morena.piem@outlook.com>
2023-04-12 16:25:46 +0200

    FEAT trains table

--- database/migrations/2023_04_12_140410_create_trains_table.php
+++ database/migrations/2023_04_12_140410_create_trains_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('num_carriages');
             $table->timestamps();
         });
     }
@@ -26,6 +33,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trains');
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn(['agency', 'departure_station', 'arrival_station', 'departure_time', 'arrival_time', 'train_code', 'num_carriages']);
+        });
     }
 };
